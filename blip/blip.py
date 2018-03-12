2fd7100738f0ed5159ae36074c2066da38b178b1
weatherman2095 <weatherman2095@users.noreply.github.com>
2018-03-12 15:51:34 -0400

    Use built-in argparse output management for program output

--- blip/blip.py
+++ blip/blip.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sys import argv
+from sys import argv, stdout, stderr
 import argparse
 
 __version__ = "1.0.0"
@@ -12,7 +12,10 @@ def parse_args(args=None):
     device_group.add_argument('--device', '-d', type=str, metavar="DEV", nargs=1, default=None, help="Device from which to capture input")
     device_group.add_argument('--pcap-input', '-p', type=str, nargs=1, metavar="CAPFILE", default=None, help="PCAP file from which to read input")
 
-    argparser.add_argument('--output','-o', nargs=1, metavar='FILE', help="Write binary output to FILE instead of stdout")
+    argparser.add_argument('--output','-o', nargs='?',
+                           type=argparse.FileType('w'),
+                           metavar='FILE', help="Write binary output to FILE instead of stdout",
+                           default=stdout, const=stdout)
     argparser.add_argument('--version', '-v', action='version', version="{} {}".format('%(prog)s', __version__))
     argparser.add_argument('--limit', '-l', nargs=1, type=int, metavar="NUM", help="Only capture NUM packets")
     return argparser.parse_args(args) if args is not None else argparser.parse_args() # Allow REPL debugging with arg lists
